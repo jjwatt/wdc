36cfd383775b2bc9743f6063bfd8a5011a99162a
Jesse Wattenbarger <jwatt@broken.watch>
2025-05-04 10:43:36 -0400

    Working separate files
    I think I still need to clean up and figure out compilation units

--- src/wdc.c
+++ src/wdc.c
@@ -9,24 +9,7 @@
 #define NOB_STRIP_PREFIX
 #include "nob.h"
 
-#define DELIM "|"
-#define BM_FILENAME ".wdc"
-#define MAX_LINE_LENGTH (PATH_MAX + 50)
-
-typedef struct {
-    String_Builder *items;
-    size_t count;
-    size_t capacity;
-} Bookmarks;
-
-
-void print_usage(const char *program_name) {
-    fprintf(stderr, "Usage: %s [OPTIONS]\n", program_name);
-    fprintf(stderr, "Options:\n");
-    fprintf(stderr, "  -h, --help          Show this help message and exit\n");
-    fprintf(stderr, "  -l, --list          List bookmarks\n");
-    fprintf(stderr, "  -a, --add [name]    Add current directory with name\n");
-}
+#include "wdc.h"
 
 FILE *open_bookmark_file(const char *mode) {
     char bookmark_path[PATH_MAX];
@@ -73,7 +56,6 @@ int add(const char *name) {
     return 0;
 }
 
-
 /**
  * @brief Get bookmarks
  *
@@ -81,7 +63,7 @@ int add(const char *name) {
  *
  * @return Bookmarks All the bookmarks from the file.
  */
-Bookmarks get_bookmarks() {
+Bookmarks get_bookmarks(void) {
     Bookmarks bookmarks = {0};
     String_Builder file_chars = {0};
 
@@ -131,7 +113,7 @@ Bookmarks get_bookmarks() {
  *
  * @return Bookmarks, the list of bookmarks in reverse order.
  */
-Bookmarks get_bookmarks_reversed() {
+Bookmarks get_bookmarks_reversed(void) {
     Bookmarks bookmarks = get_bookmarks();
     // If there's none or one just return that.
     if (bookmarks.items == NULL || bookmarks.count <= 1) {
@@ -198,57 +180,3 @@ char *find(const char *name) {
     da_free(bookmarks);
     return found_path;
 }
-
-int main(int argc, char **argv) {
-    char *find_name = NULL;
-
-    int opt;
-    opterr = 0;
-
-    /* Define long options */
-    struct option long_options[] = {
-	{"help", no_argument, 0, 'h'},
-	{"list", no_argument, 0, 'l'},
-	{"add", required_argument, NULL, 'a'},
-	{0, 0, 0, 0}
-    };
-
-    /* Process command line options */
-    while ((opt = getopt_long(argc, argv, "hla:", long_options, NULL)) != -1) {
-	switch (opt) {
-	case 'h':
-	    print_usage(argv[0]);
-	    return 0;
-	case 'l':
-	    return list_bookmarks();
-	case 'a':
-	    if (add(optarg) != 0) {
-		return 1;
-	    }
-	    break;
-	default:
-	    print_usage(argv[0]);
-	    return 1;
-	}
-    }
-
-    /* Check for non-option arguments after getopt_long */
-    if (optind < argc) {
-	find_name = argv[optind];
-    }
-    if (find_name != NULL) {
-	char *path = find(find_name);
-	if (path != NULL) {
-	    printf("%s\n", path);
-	    free(path);
-	} else {
-	    // TODO: Maybe don't print anything
-	    printf("Bookmark '%s' not found\n", find_name);
-	    return 1;
-	}
-    } else if (argc == 1) {
-	print_usage(argv[0]);
-    }
-    return 0;
-}
-
