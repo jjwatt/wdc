807c3d6160558baf860b604e811231dd1f940f09
Jesse Wattenbarger <jwatt@broken.watch>
2025-05-04 12:07:43 -0400

    Build wdc as a static library in the build script
    Buils as a stic lib and then links to main.

--- nob.c
+++ nob.c
@@ -10,7 +10,15 @@ int main(int argc, char **argv) {
     if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return 1;
 
     Nob_Cmd cmd = {0};
-    nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-o", BUILD_FOLDER"wdc", SRC_FOLDER"wdc.c", SRC_FOLDER"main.c");
-    if (!nob_cmd_run_sync(cmd)) return 1;
+    /* nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-o", BUILD_FOLDER"wdc", SRC_FOLDER"wdc.c", SRC_FOLDER"main.c"); */
+    /* Build the lib object */
+    nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-c", SRC_FOLDER"wdc.c", "-o", BUILD_FOLDER"wdc.o");
+    if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
+    /* Create static lib */
+    nob_cmd_append(&cmd, "ar", "rcs", BUILD_FOLDER"libwdc.a", BUILD_FOLDER"wdc.o");
+    if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
+    /* Link static library with main */
+    nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", SRC_FOLDER"main.c", "-o", BUILD_FOLDER"wdc", "-L"BUILD_FOLDER, "-lwdc");
+    if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
     return 0;
 }
