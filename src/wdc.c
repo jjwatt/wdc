b26dc80781b1a5be58ebb0cd7887a4b63c0d70f3
Jesse Wattenbarger <jwatt@broken.watch>
2025-05-04 18:54:58 -0400

    Bring in new version of get_bookmarks() from tests/
    I'm trying to better use String_Views and String_Builders from nob.h

--- src/wdc.c
+++ src/wdc.c
@@ -65,6 +65,32 @@ int add(const char *name) {
  */
 Bookmarks get_bookmarks(void) {
     Bookmarks bookmarks = {0};
+    Nob_String_Builder file_sb = {0};
+
+    char bookmark_path[PATH_MAX];
+    snprintf(bookmark_path, sizeof(bookmark_path),
+	     "%s/%s", getenv("HOME"), BM_FILENAME);
+    if (!nob_read_entire_file(bookmark_path, &file_sb)) return bookmarks;
+
+    Nob_String_View content = {
+	.data = file_sb.items,
+	.count = file_sb.count
+    };
+
+    while (content.count > 0) {
+	Nob_String_Builder bookmark = {0};
+	Nob_String_View line = nob_sv_chop_by_delim(&content, '\n');
+	nob_sb_append_buf(&bookmark, line.data, line.count);
+	nob_sb_append_null(&bookmark);
+	nob_da_append(&bookmarks, bookmark);
+    }
+    /* nob_log(NOB_INFO, "%zu", bookmarks.count); */
+    nob_sb_free(file_sb);
+    return bookmarks;
+}
+
+Bookmarks get_bookmarks_orig(void) {
+    Bookmarks bookmarks = {0};
     Nob_String_Builder file_chars = {0};
 
     char bookmark_path[PATH_MAX];
--- tests/nob_strings/nob_strings.c
+++ tests/nob_strings/nob_strings.c
@@ -1,7 +1,6 @@
 #define NOB_IMPLEMENTATION
 #include "nob.h"
 #include <limits.h>
-#include <stdbool.h>
 
 #define DELIM "|"
 #define BM_FILENAME ".wdc"
@@ -13,15 +12,14 @@ typedef struct {
     size_t capacity;
 } Bookmarks;
 
-int main(void) {
-    bool result = 0;
+Bookmarks get_bookmarks(void) {
     Bookmarks bookmarks = {0};
     Nob_String_Builder file_sb = {0};
 
     char bookmark_path[PATH_MAX];
     snprintf(bookmark_path, sizeof(bookmark_path),
 	     "%s/%s", getenv("HOME"), BM_FILENAME);
-    if (!nob_read_entire_file(bookmark_path, &file_sb)) nob_return_defer(false);
+    if (!nob_read_entire_file(bookmark_path, &file_sb)) return bookmarks;
 
     Nob_String_View content = {
 	.data = file_sb.items,
@@ -31,52 +29,21 @@ int main(void) {
     while (content.count > 0) {
 	Nob_String_Builder bookmark = {0};
 	Nob_String_View line = nob_sv_chop_by_delim(&content, '\n');
-	/* nob_log(NOB_INFO, "`"SV_Fmt"`", SV_Arg(line)); */
 	nob_sb_append_buf(&bookmark, line.data, line.count);
 	nob_sb_append_null(&bookmark);
 	nob_da_append(&bookmarks, bookmark);
     }
     nob_log(NOB_INFO, "%zu", bookmarks.count);
-    for (size_t i = 0; i < bookmarks.count; i++) {
-	nob_log(NOB_INFO, "%s\n", bookmarks.items[i].items);
-    }
-
- defer:
     nob_sb_free(file_sb);
-    return result;
-    // nob_nob_read_entire_file will read the file into one long string builder.
-    
-    /* if (nob_read_entire_file(bookmark_path, &file_chars)) { */
-	
-	/* char *current_pos = file_chars.items; */
-	/* char *line_start = file_chars.items; */
+    return bookmarks;
+}
 
-	/* while (current_pos < file_chars.items + file_chars.count) { */
-	/*     if (*current_pos == '\n') { */
-	/* 	Nob_String_Builder line_sb = {0}; */
-	/* 	// The size is where we are at, '\n', back to the start of the line. */
-	/* 	size_t line_len = current_pos - line_start; */
-	/* 	// Add from start to '\n' to the line_sb. */
-	/* 	nob_sb_append_buf(&line_sb, line_start, line_len); */
-	/* 	// Add null to make it a cstr. */
-	/* 	nob_sb_append_null(&line_sb); */
-	/* 	// Append the line to the list of bookmarks. */
-	/* 	nob_da_append(&bookmarks, line_sb); */
-	/* 	line_start = current_pos + 1; */
-	/*     } */
-	/*     current_pos++; */
-	/* } */
-	// Handle the last line/rest of the data if it doesn't end with a newline
-	// If the start of the line is still less than the total size of the file
-	// (there's still more data to read).
-	/* if (line_start < file_chars.items + file_chars.count) { */
-	/*     Nob_String_Builder line_sb = {0}; */
-	/*     size_t line_len = (file_chars.items + file_chars.count) - line_start; */
-	/*     nob_sb_append_buf(&line_sb, line_start, line_len); */
-	/*     nob_sb_append_null(&line_sb); */
-	/*     nob_da_append(&bookmarks, line_sb); */
-	/* } */
- /* } */
-    /* nob_sb_free(file_chars); */
+int main(void) {
+    Bookmarks bookmarks = get_bookmarks();
+    for (size_t i = 0; i < bookmarks.count; i++) {
+	printf("%s\n", bookmarks.items[i].items);
+	/* nob_log(NOB_INFO, "%s\n", bookmarks.items[i].items); */
+    }
+    nob_da_free(bookmarks);
     return 0;
 }
