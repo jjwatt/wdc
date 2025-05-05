8a92a0e4ce0d4b1475cd83b4655d8aa02af2165f
Jesse Wattenbarger <jwatt@broken.watch>
2025-05-05 08:17:58 -0400

    Making progress on using String_Builder and String_View in find

--- tests/nob_strings/nob_strings.c
+++ tests/nob_strings/nob_strings.c
@@ -33,17 +33,84 @@ Bookmarks get_bookmarks(void) {
 	nob_sb_append_null(&bookmark);
 	nob_da_append(&bookmarks, bookmark);
     }
-    nob_log(NOB_INFO, "%zu", bookmarks.count);
+    /* nob_log(NOB_INFO, "%zu", bookmarks.count); */
     nob_sb_free(file_sb);
     return bookmarks;
 }
 
-int main(void) {
+Bookmarks get_bookmarks_reversed(void) {
     Bookmarks bookmarks = get_bookmarks();
+    // If there's none or one just return that.
+    if (bookmarks.items == NULL || bookmarks.count <= 1) {
+	return bookmarks;
+    }
+
+    // Reverse the list.
+    // Left side.
+    size_t i = 0;
+    // Rigth side.
+    size_t j = bookmarks.count - 1;
+    while (i < j) {
+	// Swap items[i] and items[j]
+	Nob_String_Builder tmp = bookmarks.items[i];
+	bookmarks.items[i] = bookmarks.items[j];
+	bookmarks.items[j] = tmp;
+	i++;
+	j--;
+    }
+    return bookmarks;
+}
+
+char *find(const char *needle) {
+    // Search from the bottom. So, it's the last one added.
+    Bookmarks bookmarks = get_bookmarks_reversed();
+    char *found_path = NULL;
+
+    // Simple linear search. Items can repeat. We get the last one added.
+    nob_log(NOB_INFO, "Looking for: %s", needle);
+    nob_log(NOB_INFO, "needle len: %lu", strlen(needle));
     for (size_t i = 0; i < bookmarks.count; i++) {
-	printf("%s\n", bookmarks.items[i].items);
-	/* nob_log(NOB_INFO, "%s\n", bookmarks.items[i].items); */
+	Nob_String_Builder entry_sb = bookmarks.items[i];
+	/* char *entry = entry_sb.items; */
+	Nob_String_View entry_sv = nob_sb_to_sv(entry_sb);
+	Nob_String_View name_sv = nob_sv_chop_by_delim(&entry_sv, '|');
+
+	Nob_String_Builder name_sb = {0};
+	nob_sb_append_buf(&name_sb, name_sv.data, name_sv.count);
+	nob_sb_append_null(&name_sb);
+	printf("name_sb.items: %s\n", name_sb.items);
+	printf("name_sb.count: %zu\n", name_sb.count);
+	printf("strlen(name_sb.items): %lu\n", strlen(name_sb.items));
+	size_t name_sb_len = strlen(name_sb.items);
+	if (strncmp(needle, name_sb.items, name_sb_len) == 0 && name_sb_len == strlen(needle)) {
+	    printf("Found!\n");
+	}
+	/* Nob_String_Builder path_sb = {0}; */
+	/* nob_sb_append_buf(&path_sb, path.data, path.count); */
+	/* nob_sb_append_null(&path_sb); */
+	/* char *delimiter = strstr(entry, DELIM); */
+	/* if (delimiter != NULL) { */
+	/*     size_t name_len = delimiter - entry; */
+	/*     if (strncmp(entry, name, name_len) == 0 && name_len == strlen(name)) { */
+	/* 	found_path = strdup(delimiter + strlen(DELIM)); */
+	/* 	break; */
+	/*     } */
+	/* } */
+	nob_sb_free(entry_sb);
     }
     nob_da_free(bookmarks);
+    return found_path;
+}
+
+
+int main(void) {
+    /* Bookmarks bookmarks = get_bookmarks(); */
+    /* for (size_t i = 0; i < bookmarks.count; i++) { */
+    /* 	printf("%s\n", bookmarks.items[i].items); */
+    /* 	/\* nob_log(NOB_INFO, "%s\n", bookmarks.items[i].items); *\/ */
+    /* } */    
+    /* nob_da_free(bookmarks); */
+    char *path = find("wdc");
+    printf("path: %s\n", path);
     return 0;
 }
