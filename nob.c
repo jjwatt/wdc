4ec9f6868d2e6fd02f9d6365aebb3a3b5655e202
Jesse Wattenbarger <jwatt@broken.watch>
2025-05-06 08:32:35 -0400

    Build and run my one test from nob

--- nob.c
+++ nob.c
@@ -4,6 +4,11 @@
 
 #define BUILD_FOLDER "build/"
 #define SRC_FOLDER "src/"
+#define TESTS_FOLDER "tests/"
+
+const char *test_names[] = {
+    "test_add"
+};
 
 bool build_wdc_lib(Nob_Cmd *cmd) {
     nob_cmd_append(cmd, "cc", "-Wall", "-Wextra", "-c", SRC_FOLDER"wdc.c", "-o", BUILD_FOLDER"wdc.o");
@@ -22,6 +27,19 @@ bool build_wdc_main(Nob_Cmd *cmd) {
     return true;
 }
 
+bool build_and_run_test(Nob_Cmd *cmd, const char *test_name) {
+    size_t mark = nob_temp_save();
+    const char *bin_path = nob_temp_sprintf("%s%s", BUILD_FOLDER, test_name);
+    const char *src_path = nob_temp_sprintf("%s%s.c", TESTS_FOLDER, test_name);
+
+    build_wdc_lib(cmd);
+    nob_cmd_append(cmd, "cc", "-Wall", "-Wextra", src_path, "-o", bin_path, "-L"BUILD_FOLDER, "-lwdc");
+    if (!nob_cmd_run_sync_and_reset(cmd)) return false;
+    nob_cmd_append(cmd, bin_path);
+    if (!nob_cmd_run_sync_and_reset(cmd)) return false;
+    return true;
+}
+
 int main(int argc, char **argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
     if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return 1;
@@ -31,9 +49,7 @@ int main(int argc, char **argv) {
     /* nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-c", SRC_FOLDER"wdc.c", "-o", BUILD_FOLDER"wdc.o"); */
     /* if (!nob_cmd_run_sync_and_reset(&cmd)) return 1; */
     build_wdc_lib(&cmd);
-
-    /* Link static library with main */
-    nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", SRC_FOLDER"main.c", "-o", BUILD_FOLDER"wdc", "-L"BUILD_FOLDER, "-lwdc");
-    if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
+    build_wdc_main(&cmd);
+    build_and_run_test(&cmd, "test_add");
     return 0;
 }
