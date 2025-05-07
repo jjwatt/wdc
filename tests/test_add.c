ce9d6a294a23381b5188c363de78eb2ae19774f5
Jesse Wattenbarger <jwatt@broken.watch>
2025-05-07 19:49:36 -0400

    Build both tests and use WDC_BOOKMARK_FILE env var

--- tests/test_add.c
+++ tests/test_add.c
@@ -1,18 +1,17 @@
 #include "greatest.h"
 #include "../src/wdc.h"
 
-// TODO: Use mktemp
-#define BM_FILENAME "/tmp/test.wdc"
-
 TEST add_should_work(void) {
-  ASSERT_EQ(0, add("test"));
-  PASS();
+    // TODO: use mktemp
+    setenv("WDC_BOOKMARK_FILE", "/tmp/test.wdc", 1);
+    ASSERT_EQ(0, add("test"));
+    PASS();
 }
 
 GREATEST_MAIN_DEFS();
 
 int main(int argc, char **argv) {
-  GREATEST_MAIN_BEGIN();
-  RUN_TEST(add_should_work);
-  GREATEST_MAIN_END();
+    GREATEST_MAIN_BEGIN();
+    RUN_TEST(add_should_work);
+    GREATEST_MAIN_END();
 }
--- tests/test_get_bookmark_path.c
+++ tests/test_get_bookmark_path.c
@@ -9,14 +9,13 @@ TEST get_bookmark_path_should_return_path(void) {
     char *home_dir = getenv("HOME");
     char buf[PATH_MAX];
     snprintf(buf, PATH_MAX, "%s/%s", home_dir, BM_FILENAME);
-    printf("sb.count: %zu\n", sb.count);
-    printf("strlen(sb.items): %lu\n", strlen(sb.items));
-    printf("strlen(buf): %lu\n", strlen(buf));
     ASSERT_STR_EQ(buf, sb.items);
+
     sb.count = 0;
     setenv("WDC_BOOKMARK_FILE", "/tmp/test.wdc", 1);
     sb = get_bookmark_path();
     printf("%s\n", sb.items);
+    ASSERT_STR_EQ("/tmp/test.wdc", sb.items);
     PASS();
 }
 
