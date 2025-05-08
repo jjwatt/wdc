2ce657c9770abefc462c594eb25990d3d45b05e1
Jesse Wattenbarger <jwatt@broken.watch>
2025-05-08 06:58:13 -0400

    Tests helped me fix get_bookmark_path

--- tests/test_add.c
+++ tests/test_add.c
@@ -2,35 +2,27 @@
 #include "greatest.h"
 #include "../src/wdc.h"
 
+
 TEST add_should_work(void) {
-    // TODO: use mkdtemp
+    // TODO: add "get_tempdir"
     char template[] = "/tmp/wdc-XXXXXX";
-    char *tempdir = mkdtemp(template);
-    printf("tempdir: %s\n", tempdir);
+    char *temp_dir = mkdtemp(template);
+    printf("tempdir: %s\n", temp_dir);
     size_t mark = nob_temp_save();
-    const char *temp_path = nob_temp_sprintf("%s/%s", tempdir, "test.wdc");
+    const char *temp_path = nob_temp_sprintf("%s/%s", temp_dir, "test.wdc");
     printf("temp_path: %s\n", temp_path);
     setenv("WDC_BOOKMARK_FILE", temp_path, 1);
+    printf("env: %s\n", getenv("WDC_BOOKMARK_FILE"));
     ASSERT_EQ(0, add("test"));
+    list_bookmarks();
     nob_temp_rewind(mark);
     PASS();
 }
-
-TEST list_bookmarks_should_work(void) {
-    setenv("WDC_BOOKMARK_FILE", "/tmp/test.wdc", 1);
-    Nob_String_Builder sb;
-    sb = get_bookmark_path();
-    printf("bookmark path: %s\n", sb.items);
-    list_bookmarks();
-    PASS();
-}
-
 
 GREATEST_MAIN_DEFS();
 
 int main(int argc, char **argv) {
     GREATEST_MAIN_BEGIN();
     RUN_TEST(add_should_work);
-    RUN_TEST(list_bookmarks_should_work);
     GREATEST_MAIN_END();
 }
