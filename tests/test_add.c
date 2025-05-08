526c30ed4344c13035867d5dfdfc102aadbc5943
Jesse Wattenbarger <jwatt@broken.watch>
2025-05-08 05:42:45 -0400

    Use mkdtemp to create a temp file in test

--- tests/test_add.c
+++ tests/test_add.c
@@ -1,10 +1,18 @@
+#include <stdlib.h>
 #include "greatest.h"
 #include "../src/wdc.h"
 
 TEST add_should_work(void) {
-    // TODO: use mktemp
-    setenv("WDC_BOOKMARK_FILE", "/tmp/test.wdc", 1);
+    // TODO: use mkdtemp
+    char template[] = "/tmp/wdc-XXXXXX";
+    char *tempdir = mkdtemp(template);
+    printf("tempdir: %s\n", tempdir);
+    size_t mark = nob_temp_save();
+    const char *temp_path = nob_temp_sprintf("%s/%s", tempdir, "test.wdc");
+    printf("temp_path: %s\n", temp_path);
+    setenv("WDC_BOOKMARK_FILE", temp_path, 1);
     ASSERT_EQ(0, add("test"));
+    nob_temp_rewind(mark);
     PASS();
 }
 
