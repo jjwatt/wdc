23828a7b36e2a641cbde02ec51933108a11ceb48
Jesse Wattenbarger <jwatt@broken.watch>
2025-05-11 22:04:15 -0400

    Add BUILD_ASAN option and "test" and "rebuild" subcommands
    - Add BUILD_ASAN option to build with address sanitizer
    - Add "test" and "rebuild" subcommands to nob.c
    - Add assertion for pop test
    - Add new add_many_should_add test

--- nob.c
+++ nob.c
@@ -22,8 +22,11 @@ bool build_wdc_lib(Nob_Cmd *cmd) {
 	nob_da_free(source_paths);
 	return true;
     }
-    nob_cmd_append(cmd, "cc", "-Wall", "-Wextra","-fsanitize=address", "-g", "-c", src_path, "-o", bin_path);
-    /* nob_cmd_append(cmd, "cc", "-Wall", "-Wextra", "-g", "-c", src_path, "-o", bin_path); */
+    if (getenv("BUILD_ASAN")) {
+	nob_cmd_append(cmd, "cc", "-Wall", "-Wextra","-fsanitize=address", "-g", "-c", src_path, "-o", bin_path);
+    } else {
+	nob_cmd_append(cmd, "cc", "-Wall", "-Wextra", "-g", "-c", src_path, "-o", bin_path);
+    }
     if (!nob_cmd_run_sync_and_reset(cmd)) return false;
     /* Create static lib */
     nob_cmd_append(cmd, "ar", "rcs", BUILD_FOLDER"libwdc.a", bin_path);
@@ -48,8 +51,11 @@ bool build_wdc_main(Nob_Cmd *cmd) {
 	return true;
     }
     /* Link static library with main */
-    nob_cmd_append(cmd, "cc", "-Wall", "-Wextra", "-fsanitize=address", "-g", src_path, "-o", bin_path, "-L"BUILD_FOLDER, "-lwdc");
-    /* nob_cmd_append(cmd, "cc", "-Wall", "-Wextra", "-g", src_path, "-o", bin_path, "-L"BUILD_FOLDER, "-lwdc"); */
+    if (getenv("BUILD_ASAN")) {
+	nob_cmd_append(cmd, "cc", "-Wall", "-Wextra", "-fsanitize=address", "-g", src_path, "-o", bin_path, "-L"BUILD_FOLDER, "-lwdc");
+    } else {
+	nob_cmd_append(cmd, "cc", "-Wall", "-Wextra", "-g", src_path, "-o", bin_path, "-L"BUILD_FOLDER, "-lwdc");
+    }
     if (!nob_cmd_run_sync_and_reset(cmd)) return false;
     nob_da_free(source_paths);
     return true;
@@ -61,8 +67,11 @@ bool build_and_run_test(Nob_Cmd *cmd, const char *test_name) {
     const char *src_path = nob_temp_sprintf("%s%s.c", TESTS_FOLDER, test_name);
 
     build_wdc_lib(cmd);
-    nob_cmd_append(cmd, "cc", "-Wall", "-Wextra", "-fsanitize=address", "-g", src_path, "-o", bin_path, "-L"BUILD_FOLDER, "-lwdc");
-    /* nob_cmd_append(cmd, "cc", "-Wall", "-Wextra", "-g", src_path, "-o", bin_path, "-L"BUILD_FOLDER, "-lwdc"); */
+    if (getenv("BUILD_ASAN")) {
+	nob_cmd_append(cmd, "cc", "-Wall", "-Wextra", "-fsanitize=address", "-g", src_path, "-o", bin_path, "-L"BUILD_FOLDER, "-lwdc");
+    } else {
+	nob_cmd_append(cmd, "cc", "-Wall", "-Wextra", "-g", src_path, "-o", bin_path, "-L"BUILD_FOLDER, "-lwdc");
+    }
     if (!nob_cmd_run_sync_and_reset(cmd)) return false;
     nob_cmd_append(cmd, bin_path);
     if (!nob_cmd_run_sync_and_reset(cmd)) return false;
@@ -72,13 +81,25 @@ bool build_and_run_test(Nob_Cmd *cmd, const char *test_name) {
 
 int main(int argc, char **argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
-    if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return 1;
 
     Nob_Cmd cmd = {0};
+
+    const char *program_name = nob_shift(argv, argc);
+    const char *command_name = "";
+    if (argc > 0) command_name = nob_shift(argv, argc);
+    if (strcmp(command_name, "rebuild") == 0) {
+	nob_cmd_append(&cmd, "rm", "-rf", BUILD_FOLDER);
+	if (!nob_cmd_run_sync_and_reset(&cmd)) return false;
+    }
+    if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return 1;
+
     /* Build the lib */
     if (!build_wdc_lib(&cmd)) return 1;
     if (!build_wdc_main(&cmd)) return 1;
-    if (!build_and_run_test(&cmd, "test_get_bookmark_path")) return 1;
-    if (!build_and_run_test(&cmd, "test_add_and_get_bookmarks")) return 1; 
+
+    if (strcmp(command_name, "test") == 0) {
+	/* if (!build_and_run_test(&cmd, "test_get_bookmark_path")) return 1; */
+	if (!build_and_run_test(&cmd, "test_add_and_get_bookmarks")) return 1;
+    }
    return 0;
 }
